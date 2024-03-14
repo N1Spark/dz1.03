cd585a44442dd07bb93f47d075e52de0f0a215d4
N1Spark <112936559+N1Spark@users.noreply.github.com>
2024-03-14 21:48:30 +0200

    Add files via upload

---
+++ dz_1.03/dz_1.03/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace dz_1._03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(17, 181);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 26);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Добавить книгу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(16, 160);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(317, 28);
+            this.button6.TabIndex = 32;
+            this.button6.Text = "Поиск по издателю";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 124);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(317, 28);
+            this.button5.TabIndex = 31;
+            this.button5.Text = "Поиск по категории";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 88);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(317, 28);
+            this.button4.TabIndex = 30;
+            this.button4.Text = "Поиск по автору";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 52);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(317, 28);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Поиск по названию";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(17, 22);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(316, 22);
+            this.textBox8.TabIndex = 27;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(674, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 223);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавление";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 142);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 16);
+            this.label10.TabIndex = 47;
+            this.label10.Text = "Автор";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 112);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 16);
+            this.label9.TabIndex = 46;
+            this.label9.Text = "Кол-во странниц";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 16);
+            this.label8.TabIndex = 45;
+            this.label8.Text = "Издательство";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 16);
+            this.label7.TabIndex = 44;
+            this.label7.Text = "Категория";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 16);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "Название";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(143, 142);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(190, 22);
+            this.textBox5.TabIndex = 42;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(143, 112);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(190, 22);
+            this.textBox4.TabIndex = 41;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(143, 82);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(190, 22);
+            this.textBox3.TabIndex = 40;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 53);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 22);
+            this.textBox2.TabIndex = 39;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 22);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.TabIndex = 38;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(674, 254);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(356, 215);
+            this.groupBox2.TabIndex = 34;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Поиск";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Location = new System.Drawing.Point(1036, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(288, 125);
+            this.groupBox3.TabIndex = 35;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Удаление";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Введите название книги для удаления";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(16, 82);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 28);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(17, 52);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(263, 22);
+            this.textBox7.TabIndex = 21;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox6);
+            this.groupBox4.Location = new System.Drawing.Point(12, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(656, 519);
+            this.groupBox4.TabIndex = 36;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Результат";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox6.Location = new System.Drawing.Point(7, 22);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(636, 479);
+            this.textBox6.TabIndex = 2;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1036, 148);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(288, 34);
+            this.button7.TabIndex = 37;
+            this.button7.Text = "Показать все книги";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 536);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ dz_1.03/dz_1.03/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using EagerLoadinf_Library_ADO_Winforms.Model;
+
+namespace dz_1._03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ShowAll();
+        }
+
+        public void ShowAll()
+        {
+            textBox6.Text = "";
+            using (LibraryEagerEntities bc = new LibraryEagerEntities())
+            {
+                foreach (var info in bc.Books.Include("Autors").Include("Categories").Include("Publishings"))
+                {
+                    string authors = string.Join(", ", info.Autors.Name);
+                    string categories = string.Join(", ", info.Categories.Name);
+                    string publishings = string.Join(", ", info.Publishings.Name);
+                    textBox6.AppendText($"Name:{info.Name}\r\n" +
+                        $"Autor:{authors}\r\n" +
+                        $"Page:{info.PageCount}\r\n" +
+                        $"Categories:{categories}\r\n" +
+                        $"Publishings:{publishings}\r\n\r\n");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (LibraryEagerEntities db = new LibraryEagerEntities())
+            {
+                Categories categories = db.Categories.FirstOrDefault(c => c.Name == textBox2.Text);
+                if (categories == null)
+                {
+                    categories = new Categories { Name = textBox2.Text };
+                    db.Categories.Add(categories);
+                }
+                else
+                    db.Entry(categories).Collection(c => c.Books).Load();
+                Publishings publisher = db.Publishings.FirstOrDefault(p => p.Name == textBox3.Text);
+                if (publisher == null)
+                {
+                    publisher = new Publishings { Name = textBox3.Text };
+                    db.Publishings.Add(publisher);
+                }
+                else
+                    db.Entry(publisher).Collection(p => p.Books).Load();
+                Autors author = db.Autors.FirstOrDefault(a => a.Name == textBox5.Text);
+                if (author == null)
+                {
+                    author = new Autors { Name = textBox5.Text };
+                    db.Autors.Add(author);
+                }
+                else
+                    db.Entry(author).Collection(a => a.Books).Load();
+                Books book = new Books
+                {
+                    Name = textBox1.Text,
+                    Categories = categories,
+                    Publishings = publisher,
+                    PageCount = Convert.ToInt32(textBox4.Text),
+                    Autors = author
+                };
+                db.Books.Add(book);
+                db.SaveChanges();
+                ShowAll();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (LibraryEagerEntities db = new LibraryEagerEntities())
+            {
+                Books books = db.Books.FirstOrDefault(n => n.Name == textBox7.Text);
+                if (books != null)
+                {
+                    db.Books.Remove(books);
+                    db.SaveChanges();
+                }
+                else
+                    MessageBox.Show("Не удалось найти книгу");
+                ShowAll();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (LibraryEagerEntities db = new LibraryEagerEntities())
+            {
+                Autors autor = db.Autors.FirstOrDefault(n => n.Name.Contains(textBox8.Text));
+                if (autor != null)
+                {
+                    db.Entry(autor).Collection(a => a.Books).Load();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    foreach (var book in autor.Books)
+                        stringBuilder.AppendLine($"Name:{book.Name},Page:{book.PageCount}\n");
+                    textBox6.Text = stringBuilder.ToString();
+                }
+                else
+                    textBox6.Text = "Нет результатов";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (LibraryEagerEntities db = new LibraryEagerEntities())
+            {
+                Books book = db.Books.FirstOrDefault(b => b.Name.Contains(textBox8.Text));
+                if (book != null)
+                {
+                    db.Entry(book).Reference(b => b.Categories).Load();
+                    db.Entry(book).Reference(b => b.Publishings).Load();
+                    db.Entry(book).Reference(b => b.Autors).Load();
+                    textBox6.Text = $"Название: {book.Name}\r\n" +
+                                       $"Категория: {book.Categories.Name}\r\n" +
+                                       $"Издательство: {book.PublisherId}\r\n" +
+                                       $"Количество страниц: {book.PageCount}\r\n" +
+                                       $"Автор: {book.Autors.Name}\r\n";
+                }
+                else
+                    textBox6.Text = "Нет результатов";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (LibraryEagerEntities db = new LibraryEagerEntities())
+            {
+                var books = db.Books.Where(b => b.PageCount == Convert.ToInt32(textBox4.Text)).ToList();
+                if (books.Any())
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var book in books)
+                        sb.AppendLine($"Название: {book.Name}\r, Кол-во страниц:{book.PageCount}");
+                    textBox6.Text = sb.ToString();
+                }
+                else
+                    textBox6.Text = "Нет результатов";
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (LibraryEagerEntities db = new LibraryEagerEntities())
+            {
+                Publishings publisher = db.Publishings.FirstOrDefault(p => p.Name.Contains(textBox8.Text));
+                if (publisher != null)
+                {
+                    db.Entry(publisher).Collection(p => p.Books).Load();
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var book in publisher.Books)
+                        sb.AppendLine($"Name:{book.Name}");
+                    textBox6.Text = sb.ToString();
+                }
+                else
+                    textBox6.Text = "Нет результатов";
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ShowAll();
+        }
+    }
+}
